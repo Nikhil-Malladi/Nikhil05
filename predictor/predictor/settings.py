009164cf5048ec6b44aaaa072e2db9df7c3e008f
nikhil <malladi.vsnikhil@gmail.com>
2022-11-23 15:58:47 +0530

    changed debug flag as false

--- predictor/predictor/settings.py
+++ predictor/predictor/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d(am@ypk0iy_v%9$t!bs*vcaz@f_1bcy(dguzq!+v8sk0mhqdm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
